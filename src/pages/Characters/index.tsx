af31c20084f2ec426713b360dbad021951765491
SoldadoM43 <d.novaes824@gmail.com>
2022-03-22 18:57:31 -0300

    first commit

---
+++ src/pages/Characters/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Characters: React.FC = () => {
+  return <div />;
+}
+
+export default Characters;
---
+++ src/pages/Locations/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Locations: React.FC = () => {
+  return <div>Locations</div>
+}
+
+export default Locations;
